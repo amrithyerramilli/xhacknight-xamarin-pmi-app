17f5dd573eeaf7309a3d13f3cdbcfede69313c8f
Amrith Yerramilli <amrith.yerramilli@gmail.com>
2015-09-15 17:42:01 +0530

    renaming, AppUsers logic

--- Match.AI/Match.AI/App.cs
+++ Match.AI/Match.AI/App.cs
@@ -12,50 +12,47 @@ namespace Match.AI
 {
     public class App : Application
     {
-        public static User _User;
-        public static OAuthSettings _OAuthSettings;
-        public static HomePage _HomePage;
-        public static NavigationPage _NavPage;
-        static string _Token;
-        public static string Token
-        {
-            get { return _Token; }
-        }
+        public static User User;
+        public static OAuthSettings OAuthSettings;
+        public static HomePage HomePage;
+        public static NavigationPage NavPage;
+        public static List<User> AppUsers; 
+
+        public static string Token { get; private set; }
 
         public App()
         {
             // The root page of your application
-            _OAuthSettings = new OAuthSettings(
+            OAuthSettings = new OAuthSettings(
                                     clientId: "",
                                     scope: "public_profile,email,user_friends,user_about_me,user_status",
                                     authorizeUrl: "https://m.facebook.com/dialog/oauth/",
                                     redirectUrl: "http://www.facebook.com/connect/login_success.html");
+            AppUsers = InitializeUsers();
 
-            _HomePage = new HomePage();
-            _NavPage = new NavigationPage(_HomePage);
+            HomePage = new HomePage();
+            NavPage = new NavigationPage(HomePage);
 
-            MainPage = _NavPage;
+            MainPage = NavPage;
         }
 
         public static Action SuccessfulLoginAction
         {
             get
             {
-                return new Action(() =>
+                return () =>
                 {
-                    _NavPage.Navigation.PopModalAsync();
-                });
+                    NavPage.Navigation.PopModalAsync();
+                };
             }
         }
         public static void SaveToken(string token)
         {
-            _Token = token;
+            Token = token;
             ApiClient.GetUserInfo();
             // broadcast a message that authentication was successful
-            _HomePage.Notify("Authenticated");           
+            HomePage.Notify("Authenticated");           
         }
-        
-        
 
         protected override void OnStart()
         {
@@ -71,5 +68,48 @@ namespace Match.AI
         {
             // Handle when your app resumes
         }
+        public List<User> InitializeUsers()
+        {
+            List<User> appUsers = new List<User>();
+            var baseUrl = "https://s3.amazonaws.com/rs.adx.io/test/";
+            appUsers.Add(new User()
+            {
+                name = "Steve Jobs",
+                picture = new Picture() { data = new Data() { url = baseUrl + "john.png" } },
+                personality = "", // TODO : get actual personality
+            });
+            appUsers.Add(new User()
+            {
+                name = "Marilyn Monroe",
+                picture = new Picture() { data = new Data() { url = baseUrl + "marlyn.png" } },
+                personality = "", // TODO : get actual personality
+            });
+            appUsers.Add(new User()
+            {
+                name = "Hitler",
+                picture = new Picture() { data = new Data() { url = baseUrl + "hitler.png" } },
+                personality = "", // TODO : get actual personality
+            });
+            appUsers.Add(new User()
+            {
+                name = "Narendra Modi",
+                picture = new Picture() { data = new Data() { url = baseUrl + "namo.png" } },
+                personality = "", // TODO : get actual personality
+            });
+            appUsers.Add(new User()
+            {
+                name = "Barack Obama",
+                picture = new Picture() { data = new Data() { url = baseUrl + "obama.png" } },
+                personality = "", // TODO : get actual personality
+            });
+            appUsers.Add(new User()
+            {
+                name = "Michelle Obama",
+                picture = new Picture() { data = new Data() { url = baseUrl + "michelle.png" } },
+                personality = "", // TODO : get actual personality
+            });
+
+            return appUsers;
+        } 
     }
 }
