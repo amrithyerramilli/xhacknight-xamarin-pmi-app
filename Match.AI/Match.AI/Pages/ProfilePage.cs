edd6c24698cab59922e3fb0d150aaed1253dd920
Amrith Yerramilli <amrith.yerramilli@gmail.com>
2015-09-15 11:14:07 +0530

    updated the profile page with a cool layout

--- Match.AI/Match.AI/Pages/ProfilePage.cs
+++ Match.AI/Match.AI/Pages/ProfilePage.cs
@@ -9,21 +9,106 @@ namespace Match.AI.Pages
 {
     public class ProfilePage : ContentPage
     {
-        public User myUser;
         public ProfilePage()
         {
-            var nameLabel = new Label();
-            //myUser = App._User.name
-            //nameLabel.Text = App._User != null ? App._User.name : "Not ready yet. Please go back try again in a little while.";
-            nameLabel.SetBinding(Label.TextProperty, "name");
-            nameLabel.BindingContext = App._User;
-            Content = new StackLayout()
+            // set the binding context for the entire page to the App user
+            BindingContext = App._User;
+
+            // design the profile page layout
+            // based on : https://www.syntaxismyui.com/xamarin-forms-in-anger-phoenix-peaks/
+
+            AbsoluteLayout peakLayout = new AbsoluteLayout
             {
-                Children = {
-                        nameLabel
-                    }
+                HeightRequest = 250,
+                BackgroundColor = Color.Black
             };
 
+            var title = new Label
+            {
+                //Text = "South Mountain",
+                FontSize = 30,
+                FontFamily = "AvenirNext-DemiBold",
+                TextColor = Color.White
+            };
+            title.SetBinding(Label.TextProperty, "name");
+
+            var where = new Label
+            {
+                Text = "Phoenix, AZ 85044",
+                TextColor = Color.FromHex("#ddd"),
+                FontFamily = "AvenirNextCondensed-Medium"
+            };
+            //where.SetBinding(Label.TextProperty, "name");
+
+            var image = new Image()
+            {
+                //Source = ImageSource.FromFile("southmountain.jpg"),
+                Aspect = Aspect.AspectFill,
+            };
+            image.SetBinding(Image.SourceProperty, "profilePicture");
+
+            var overlay = new BoxView()
+            {
+                Color = Color.Black.MultiplyAlpha(.7f)
+            };
+
+            var pin = new Image()
+            {
+                //Source = ImageSource.FromFile("pin.png"),
+                HeightRequest = 25,
+                WidthRequest = 25
+            };
+
+            var descriptionLabel = new Label()
+            {
+                FontSize = 14,
+                TextColor = Color.FromHex("#ddd"),
+            };
+            descriptionLabel.SetBinding(Label.TextProperty, "bio");
+            var description = new Frame()
+            {
+                Padding = new Thickness(10, 5),
+                HasShadow = false,
+                BackgroundColor = Color.Transparent,
+                Content = descriptionLabel
+            };
+
+            AbsoluteLayout.SetLayoutFlags(overlay, AbsoluteLayoutFlags.All);
+            AbsoluteLayout.SetLayoutBounds(overlay, new Rectangle(0, 1, 1, 0.3));
+
+            AbsoluteLayout.SetLayoutFlags(image, AbsoluteLayoutFlags.All);
+            AbsoluteLayout.SetLayoutBounds(image, new Rectangle(0f, 0f, 1f, 1f));
+
+            AbsoluteLayout.SetLayoutFlags(title, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(title,
+                new Rectangle(0.1, 0.85, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize)
+                );
+
+            AbsoluteLayout.SetLayoutFlags(where, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(where,
+                new Rectangle(0.1, 0.95, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize)
+                );
+
+            AbsoluteLayout.SetLayoutFlags(pin, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(pin,
+                new Rectangle(0.95, 0.9, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize)
+                );
+
+            peakLayout.Children.Add(image);
+            peakLayout.Children.Add(overlay);
+            peakLayout.Children.Add(title);
+            peakLayout.Children.Add(where);
+            peakLayout.Children.Add(pin);
+
+            Content = new StackLayout()
+            {
+                BackgroundColor = Color.FromHex("#333"),
+                Children =
+                {
+                    peakLayout,
+                    description,
+                }
+            };
         }
     }
 }
