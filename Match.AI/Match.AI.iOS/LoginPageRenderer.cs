0a0d87a185b5c63ad2fe8fe62970c0281c4d1cdd
Amrith Yerramilli <amrith.yerramilli@gmail.com>
2015-09-15 11:19:51 +0530

    updated the LoginPageRenderer for iOS

--- Match.AI/Match.AI.iOS/LoginPageRenderer.cs
+++ Match.AI/Match.AI.iOS/LoginPageRenderer.cs
@@ -13,35 +13,47 @@ namespace Match.AI.iOS
 {
     public class LoginPageRenderer : PageRenderer
     {
+
+        private bool IsShown;
+
         public override void ViewDidAppear(bool animated)
         {
             base.ViewDidAppear(animated);
 
-            var auth = new OAuth2Authenticator(
-                clientId: App._OAuthSettings.ClientId, // your OAuth2 client id
-                scope: App._OAuthSettings.Scope, // the scopes for the particular API you're accessing, delimited by "+" symbols
-                authorizeUrl: new Uri(App._OAuthSettings.AuthorizeUrl), // the auth URL for the service
-                redirectUrl: new Uri(App._OAuthSettings.RedirectUrl)); // the redirect URL for the service
-
-            auth.Completed += (sender, eventArgs) =>
+            // Fixed the issue that on iOS 8, the modal wouldn't be popped.
+            // url : http://stackoverflow.com/questions/24105390/how-to-login-to-facebook-in-xamarin-forms
+            if (!IsShown)
             {
-                // We presented the UI, so it's up to us to dimiss it on iOS.
-                App.SuccessfulLoginAction.Invoke();
 
-                if (eventArgs.IsAuthenticated)
-                {
-                    // Use eventArgs.Account to do wonderful things
-                    App.SaveToken(eventArgs.Account.Properties["access_token"]);
-                }
-                else
-                {
-                    // The user cancelled
-                    // TODO : Show appropriate error message. For now, closing the oAuth popup
-                    App.SuccessfulLoginAction.Invoke();                    
-                }
-            };
+                IsShown = true;
 
-            PresentViewController(auth.GetUI(), true, null);
+                var auth = new OAuth2Authenticator(
+                    clientId: App._OAuthSettings.ClientId, // your OAuth2 client id
+                    scope: App._OAuthSettings.Scope,
+                    // The scopes for the particular API you're accessing. The format for this will vary by API.
+                    authorizeUrl: new Uri(App._OAuthSettings.AuthorizeUrl), // the auth URL for the service
+                    redirectUrl: new Uri(App._OAuthSettings.RedirectUrl)); // the redirect URL for the service
+
+                auth.Completed += (sender, eventArgs) =>
+                {
+                    // We presented the UI, so it's up to us to dimiss it on iOS.
+                    App.SuccessfulLoginAction.Invoke();
+
+                    if (eventArgs.IsAuthenticated)
+                    {
+                        // Use eventArgs.Account to do wonderful things
+                        App.SaveToken(eventArgs.Account.Properties["access_token"]);
+                    }
+                    else
+                    {
+                        // The user cancelled
+                    }
+                };
+
+                PresentViewController(auth.GetUI(), true, null);
+
+            }
+
         }
     }
 }
