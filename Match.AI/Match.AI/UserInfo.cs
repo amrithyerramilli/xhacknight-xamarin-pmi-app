05a435eef565be5e454c5e87647ee83737734013
Amrith Yerramilli <amrith.yerramilli@gmail.com>
2015-09-15 11:14:42 +0530

    updated the User class to hold the new details

--- Match.AI/Match.AI/UserInfo.cs
+++ Match.AI/Match.AI/UserInfo.cs
@@ -6,18 +6,11 @@ using System.Threading.Tasks;
 
 namespace Match.AI
 {
-    public class User
-    {
-        public string id { get; set; }
-        public string name { get; set; }
-        public Posts posts { get; set; }
-        public string bio { get; set; }
-        public string personality { get; set; }
-    }
+
     public class Datum
     {
-        public string message { get; set; }
         public string id { get; set; }
+        public string message { get; set; }
     }
 
     public class Paging
@@ -32,6 +25,35 @@ namespace Match.AI
         public Paging paging { get; set; }
     }
 
+    public class Data
+    {
+        public bool is_silhouette { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Picture
+    {
+        public Data data { get; set; }
+    }
+
+    public class User
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public Posts posts { get; set; }
+        public string bio { get; set; }
+        public Picture picture { get; set; }
+        public Xamarin.Forms.ImageSource profilePicture
+        {
+            get
+            {
+                if (picture != null && picture.data != null && picture.data.url != null)
+                    return Xamarin.Forms.ImageSource.FromUri(new Uri(picture.data.url));
+                return null;
+            }
+        }
+        public string personality { get; set; }
+    }
 
 
 }
